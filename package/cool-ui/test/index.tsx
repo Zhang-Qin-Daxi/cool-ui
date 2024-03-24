9a44613b3b73d2387fe260c9b33efe45cf9e86e6
AliceBen <37425189+Zhang-Qin-Daxi@users.noreply.github.com>
2024-03-24 23:25:26 +0800

    first commit

---
+++ package/cool-ui/test/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function test() {
+  console.log('==========Hello, cool-ui!')
+  // return (
+  //   <div>
+  //     <h1>Hello, cool-ui!</h1>
+  //   </div>
+  // );
+}
+
+export default test;
